ac5e17db5625f84a9cc593849912606174fca59d
JoseChavez98 <jose.chavez@utec.edu.pe>
2019-11-12 08:32:00 -0500

    adding query

--- src/java-app/easy-exam/src/main/java/com/company/easyexam/mapper/QuestionService.java
+++ src/java-app/easy-exam/src/main/java/com/company/easyexam/mapper/QuestionService.java
@@ -15,4 +15,7 @@ public interface QuestionService {
     public abstract List<Question> getCollectionForChallenge( int size ,List<String> tags);
     public abstract void updateRating(List<Rate> rates);
     public abstract void postQuestions(List<QuestionPosted> questions);
+    public abstract int calculateScore(List<String> questionTags, List<String> tags);
+    public abstract List<Question> filterQuestionsByDate(List<Question>questions);
+    public abstract List<Question> filterQuestionsByMatchScore(List<Question>questions,List<String>tags);
 }
