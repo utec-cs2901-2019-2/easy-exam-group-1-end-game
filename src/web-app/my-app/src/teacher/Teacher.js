8a0c554e43bc76ffb3b95576ff090d2a6a8023a9
jaoks <jaoks@jaokss-MacBook-Air.local>
2019-11-19 01:10:35 -0500

    working to download exam

--- src/web-app/my-app/src/teacher/Teacher.js
+++ src/web-app/my-app/src/teacher/Teacher.js
@@ -75,7 +75,7 @@ export default function Checkout() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const { setAuth }  = useContext(AuthContext);
-  const { tags, setQuestions, count, disable, setDisable } = useContext(TeacherContext);
+  const { tags, questions, setQuestions, count, disable, setDisable, info } = useContext(TeacherContext);
 
   const handleNext = () => {
     setActiveStep(activeStep + 1);
@@ -97,6 +97,37 @@ export default function Checkout() {
         setAuth(false);
       }
     }
+
+    if(activeStep === steps.length - 1){
+      var questionsToCompile = [];
+      for(let i = 0; i < Object.keys(questions).length; i++) {
+        questionsToCompile = questionsToCompile.concat({
+          description: questions[i].description,
+          answer: questions[i].answer
+        });
+      }
+      if(validateToken()) {
+        axios.post("http://localhost:8080/download/exam", {
+          questionsToCompileList: questionsToCompile,
+          teacherName: info.name+" "+info.last,
+          examName: info.exam,
+          universityName: info.univ
+        },{
+          headers: {
+            "Authorization" : "Bearer "+localStorage.getItem("token")
+          }
+        })
+        .then( 
+          r => {
+            console.log("download start")
+          }
+        )
+      } else {
+        alert("Tu sesión ha expirado");
+        setAuth(false);
+      }
+    }
+
     setDisable(true);
   };
 
