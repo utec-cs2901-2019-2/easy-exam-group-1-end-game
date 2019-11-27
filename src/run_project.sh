a13146822dc7ca7970c3750b29f50672136cef54
JoseChavez98 <jose.chavez@utec.edu.pe>
2019-11-27 16:01:01 -0500

    fixing project script

--- src/run_project.sh
+++ src/run_project.sh
@@ -3,9 +3,9 @@
 cd java-app/easy-exam/
 gradle build 
 
-java -jar build/libs/easy-exam-0.0.1-SNAPSHOT.jar
+java -jar build/libs/easy-exam-0.0.1-SNAPSHOT.jar &
 
 cd ../../web-app/my-app/
 npm install
-npm start
+npm start &
 
